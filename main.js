24024b12d31201644b8ed739d836edeea18be31f
Naitik tiwari <106234126+AmazeProd@users.noreply.github.com>
2023-01-01 13:05:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+console.log('Hello World!');
+a = document.getElementById("start");
+b = document.getElementById("ab");
+a.innerHTML = ""
+function l(){
+  c = document.getElementById("bas")
+b = document.getElementById ("ab");
+a = document.getElementById ("start")
+e = document.getElementById("all")
+e.style.backgroundImage = "url('212323546ba-colorful-fireworks-animated-gif-pic.gif')";
+c.innerHtml = ""
+  console.log("ok")
+  c.innerHTML = `
+  <h1 class ="op">Happy New Year ${ b.value} </h1> <p class = "x" >From Naitik Tiwari Best wishes for you in 2023</p> <p>Other Websites - 1.<a href="https://naitiktiwa.github.io/Histac--essays/">Histac</a> 2. <a href="https://naitiktiwa.github.io/mynotes/">MyNotes</a> 3. <a href="https://naitiktiwa.github.io/calculator/">Calculator</a>
+ </p>
+ 
+  `
+  console.log(b.value)
+}
+  
