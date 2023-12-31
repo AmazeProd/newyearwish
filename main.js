27e3c5cb71db0d5485b704dd105e5dc2231fd51c
Naitik tiwari <106234126+AmazeProd@users.noreply.github.com>
2023-12-31 21:21:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+console.log('Hello World!');
+a = document.getElementById("start");
+b = document.getElementById("ab");
+a.innerHTML = ""
+function l(){
+  c = document.getElementById("bas")
+b = document.getElementById ("ab");
+a = document.getElementById ("start")
+e = document.getElementById("all")
+c.style.backgroundImage = "url('happy-new-year-2024-photo-editing-background-hd-photos-11702211353ltpv3rhcyw.jpg')";
+document.body.style.backgroundImage = "url('212323546ba-colorful-fireworks-animated-gif-pic.gif')";
+
+
+c.innerHtml = ""
+  console.log("ok")
+  c.innerHTML = `
+  <h1 class ="op">${ b.value} </h1> <p class = "x" ><b>From Naitik Tiwari and Amaze Corporation Best wishes for you in 2024</b></p> <p class ="ii">Other Websites - 1.<a class ="ii" href="https://naitiktiwa.github.io/Histac--essays/">Histac</a> 2. <a class ="ii" href="https://naitiktiwa.github.io/mynotes/">MyNotes</a> 3. <a class ="ii" href="https://naitiktiwa.github.io/calculator/">Calculator</a>
+ </p>
+ 
+  `
+  console.log(b.value)
+}
+  
